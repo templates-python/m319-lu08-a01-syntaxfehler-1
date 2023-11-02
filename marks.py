89db6fecbd662a3db3d7384c94afb3ba6f12e155
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-11-02 10:34:37 +0100

    done

--- marks.py
+++ marks.py
@@ -1,24 +1,22 @@
 def main():
-    mark_count = '0'
-    mark_total = '0.0'
-    mark_minimum = '99.9'
+    mark_count = 0
+    mark_total = 0.0
+    mark_minimum = 99.9
 
-    mark = float(input('Note eingeben > ')
-    while mark > 0.0
+    mark = float(input('Note eingeben > '))
+    while mark > 0.0:
         mark_count += 1
-            mark_total += marks
+        mark_total += mark
         if mark < mark_minimum:
             mark_minimum = mark
-        mark = float(input('Note eingeben > ')))
+        mark = float(input('Note eingeben > '))
 
-    if mark_count greater_than 1:
-        mark_total =- mark_minimum
-        mark_count =- 1
-    average = mark_totaL : mark_count
+    if mark_count > 1:
+        mark_total -= mark_minimum
+        mark_count -= 1
+    average = mark_total / mark_count
 
-    print 'Notenschnitt: ' & average
-
-
+    print('Notenschnitt: ' + str(average))
 
 
 if __name__ == '__main__':
