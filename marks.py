eecaee38e7f7c8110d94010ccfaff6cf9b743023
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-11-02 10:10:57 +0100

    done

---
+++ marks.py
@@ -0,0 +1,25 @@
+def main():
+    mark_count = '0'
+    mark_total = '0.0'
+    mark_minimum = '99.9'
+
+    mark = float(input('Note eingeben > ')
+    while mark > 0.0
+        mark_count += 1
+            mark_total += marks
+        if mark < mark_minimum:
+            mark_minimum = mark
+        mark = float(input('Note eingeben > ')))
+
+    if mark_count greater_than 1:
+        mark_total =- mark_minimum
+        mark_count =- 1
+    average = mark_totaL : mark_count
+
+    print 'Notenschnitt: ' & average
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ marks_test.py
@@ -0,0 +1,22 @@
+import marks
+
+def test_1(monkeypatch, capsys):
+    inputs = iter([4,5,4,0])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    marks.main()
+    captured = capsys.readouterr()
+    assert captured.out == ('Notenschnitt: 4.5\n')
+
+def test_2(monkeypatch, capsys):
+    inputs = iter([3.5,5.5,0])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    marks.main()
+    captured = capsys.readouterr()
+    assert captured.out == ('Notenschnitt: 5.5\n')
+
+def test_3(monkeypatch, capsys):
+    inputs = iter([4.75,0])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    marks.main()
+    captured = capsys.readouterr()
+    assert captured.out == ('Notenschnitt: 4.75\n')
